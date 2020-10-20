a6808473fa477ea1ba386e1715f81d092b8ea3c8
Fib88 <nsukifrank@hotmail.com>
2020-10-20 14:26:09 +0200

    save state

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 declare(strict_types=1);
-require 'config.php';
+
+
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
+require 'config.php';
+//require "Model/resources/Products.php";
+require "ProductsLoader.php";
 echo "Testing testing";
 
 
@@ -19,6 +23,14 @@ function openConnection() : PDO {
     $pdo = new PDO('mysql:host='. Config::$dbhost .';dbname='. Config::$db, Config::$dbuser, Config::$dbpass, $driverOptions);
     return $pdo;
 }
+openConnection();
+include "ProductsLoader.php";
+$products_data = new Products(5,"zipper",2489);
+$products_list = $products_data->getName();
+
+echo "$products_list";
+
+
 
 ?>
 
