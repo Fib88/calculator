e99fdca5fa1c9dd664629409c3934436427b20ac
Fib88 <nsukifrank@hotmail.com>
2020-10-20 09:30:49 +0200

    testing web page setup

--- index.php
+++ index.php
@@ -1,19 +1,11 @@
 <?php
 declare(strict_types=1);
 
-//include all your model files here
-require 'Model/User.php';
-//include all your controllers here
-require 'Controller/HomepageController.php';
-require 'Controller/InfoController.php';
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
-//you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
-//this file should never be more than 20 lines of code!
-
-$controller = new HomepageController();
-if(isset($_GET['page']) && $_GET['page'] === 'info') {
-    $controller = new InfoController();
-}
+echo "Testing testing";
 
 
-$controller->render($_GET, $_POST);
+
