61bee437e6b0511ac16b673ed9dc9c29197f83e3
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-10-20 10:28:48 +0200

    made getters for Customer, will need a way to get discounts later too

--- Model/Customer.php
+++ Model/Customer.php
@@ -3,5 +3,43 @@
 
 class Customer
 {
+    private int $id;
+    private string $firstName;
+    private string $lastName;
+    private CustomerGroup $customerGroup;
+    //might need a variable for discount.
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @return CustomerGroup
+     */
+    public function getCustomerGroup(): CustomerGroup
+    {
+        return $this->customerGroup;
+    }
+
 
 }
