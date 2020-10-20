7a1beafd7937cf645745b28e5d899830c2045369
Fib88 <nsukifrank@hotmail.com>
2020-10-20 10:31:46 +0200

    added product class

--- Model/Products.php
+++ Model/Products.php
@@ -3,5 +3,44 @@
 
 class Products
 {
+    private int $id;
+    private string $name;
+    private int $price;
 
+    /**
+     * Products constructor.
+     * @param int $id
+     * @param string $name
+     * @param int $price
+     */
+    public function __construct(int $id, string $name, int $price)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
 }
--- index.php
+++ index.php
@@ -9,3 +9,6 @@ echo "Testing testing";
 
 
 
+
+
+
