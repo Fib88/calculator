37f89e8dd9bf48d7bf77d8d69c1b5da11be23a9a
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-10-21 14:57:31 +0200

    attempts to split firstname from lastname, fixed in homepageController

--- View/homepage.php
+++ View/homepage.php
@@ -12,16 +12,14 @@ Anything complex should be calculated in the model -->
             <?php foreach ($names as $row): ?>
                 <option><?=$row["firstname"]. " ". $row['lastname']?></option>
             <?php endforeach ?>
-
-
-            //<label for="dropdown">Select</label>
+            <label for="dropdown">Select</label>
         </select>
-       <select name="dropdown" id="dropdown">
+       <select name="dropdown2" id="dropdown2">
             <?php foreach ($rows as $row): ?>
            <option><?=$row["name"]. " €". $row['price']/100?></option>
            <?php endforeach ?>
 
-           <label for="dropdown">Select</label>
+           <label for="dropdown2">Select</label>
       </select>
       <button type="submit">Submit</button>
     </form>
