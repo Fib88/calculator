3992ce73cd290d4b578fd9bf57ef8ec70c9a7640
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-10-22 09:31:22 +0200

    moved pdo lines to connection class, tried to make things more OOP

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -18,42 +18,29 @@ whatIsHappening();
 
 class HomepageController
 {
-
-    public function openConnection(): PDO
-    {
-        // Try to figure out what these should be for you
-        $driverOptions = [
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        ];
-
-        // Try to understand what happens here
-        $pdo = new PDO('mysql:host=' . Config::$dbhost . ';dbname=' . Config::$db, Config::$dbuser, Config::$dbpass, $driverOptions);
-
-        // Why we do this here
-        return $pdo;
-    }
-
     public function findGroup($id)
     {
-        $pdo = $this->openConnection();
+        $connector = new Connection();
+        $pdo = $connector->getPdo();
         $handle = $pdo->prepare('SELECT * FROM customer_group WHERE id = :id ');
         $handle->bindValue(':id', $id);
         $handle->execute();
         return $handle->fetch();
 
     }
+
     //render function with both $_GET and $_POST vars available if it would be needed.
     public function render(array $GET, array $POST)
     {
-        $pdo = $this->openConnection();
+        $connector = new Connection();
+        $pdo = $connector->getPdo();
+
+
         $handle = $pdo->prepare('SELECT name, price FROM product ');
         $handle->execute();
         $rows = $handle->fetchAll();
 
 
-        $pdo = $this->openConnection();
         $handle = $pdo->prepare('SELECT firstname, lastname FROM customer ');
         $handle->execute();
         $names = $handle->fetchAll();
@@ -80,7 +67,7 @@ class HomepageController
         }
         $customername = explode(" ", $customerInfo);
 
-        $pdo = $this->openConnection();
+
         //split $customer info on space, keep both names
         $handle = $pdo->prepare('SELECT  * FROM customer WHERE firstname LIKE :firstname && lastname LIKE :lastname ');
         $handle->bindValue(':firstname', $customername[0]);
@@ -89,21 +76,21 @@ class HomepageController
         $handle->execute();
         $SelectedCustomer = $handle->fetchAll();
         var_dump($SelectedCustomer);
-        if (!empty($SelectedCustomer)){
+        if (!empty($SelectedCustomer)) {
 
 
-        $GroupID = $SelectedCustomer[0]['group_id'];
-        $fixedDiscount = $SelectedCustomer[0]['fixed_discount'];
+            $GroupID = $SelectedCustomer[0]['group_id'];
+            $fixedDiscount = $SelectedCustomer[0]['fixed_discount'];
 
-        $allGroups=array();
-        array_unshift($allGroups,$this->findGroup($GroupID));
+            $allGroups = array();
+            array_unshift($allGroups, $this->findGroup($GroupID));
 
-        while ($allGroups[0]['parent_id'] !== null) {
-           array_unshift($allGroups,$this->findGroup($allGroups[0]['parent_id']));
+            while ($allGroups[0]['parent_id'] !== null) {
+                array_unshift($allGroups, $this->findGroup($allGroups[0]['parent_id']));
 
-        }
-        var_dump( $allGroups);
-        echo $fixedDiscount;
+            }
+            var_dump($allGroups);
+            echo $fixedDiscount;
         }
 
         //loop over the arrays, if the name matches, get the other attributes, but customername will not match with firstname.
